b3aec624c9a1e9dc403cfc20769b940afdf96d68
iSpeX1 <estebangeley@gmail.com>
2023-12-22 11:59:10 +0100

    change properties folder

--- ListService/src/main/java/fr/insa/ListService/controller/ListController.java
+++ ListService/src/main/java/fr/insa/ListService/controller/ListController.java
@@ -2,14 +2,19 @@ package fr.insa.ListService.controller;
 
 
 import fr.insa.ListService.model.IDList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/user")
 public class ListController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/ids/{idGroup}")
     public IDList getIds(@PathVariable("idGroup") String Group) {
@@ -17,6 +22,9 @@ public class ListController {
         ids.add(1);
         ids.add(2);
         ids.add(3);
+
+        int i = 0;
+
         return ids;
     }
 
